aaf3231d90759c679495984ae687b47f4a02bcb2
yeram410 <68213891+yeram410@users.noreply.github.com>
2021-02-17 17:12:46 +0900

    frontend

---
+++ kpmg-web/server.js
@@ -0,0 +1,32 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get((req, res) => {
+  })
+  .post(async (req, res) => {
+    
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
