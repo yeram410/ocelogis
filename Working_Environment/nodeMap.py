93976ff8b43c1d3c2c3aa16362de2f5873e201fa
Joommm <21600290@handong.edu>
2021-02-19 02:18:56 +0900

    making path on map

--- Working_Environment/nodeMap.py
+++ Working_Environment/nodeMap.py
@@ -14,7 +14,7 @@ G = ox.graph_from_place('경상북도, 대한민국', network_type='drive', simp
 G_proj = ox.project_graph(G)
 
 orig_node = ox.get_nearest_node(G, (36.104095, 129.391966))
-dest_node = ox.get_nearest_node(G, (35.870975, 128.594533))
+dest_node = ox.get_nearest_node(G, (36.554066, 128.735353))
 
 
 import matplotlib as mpl
@@ -31,10 +31,8 @@ fig, ax = ox.plot_graph_route(G, route, node_size = 0, figsize=(20,20))
 len = nx.shortest_path_length(G, orig_node, dest_node, weight = 'length') / 1000
 print(round(len, 1), "킬로미터")
 
-# =============================================================================
-# #Folium 웹맵에 시각화
-# rroute_graph_map = ox.plot_route_folium(G, route,
-#        route_map=graph_map, popup_attribute='length')
-# ox.plot_route_folium()
-# route_graph_map.save('nodeMap.html')
-# =============================================================================
+#Folium 웹맵에 시각화
+route_graph_map = ox.plot_route_folium(G, route, route_map=mpl.graph_map, popup_attribute='length')
+route_graph_map.save('nodeMap.html')
+
+
