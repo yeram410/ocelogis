961dd28214df7a105bf7874eb895e01a98b5fca7
Joommm <21600290@handong.edu>
2021-02-19 20:54:31 +0900

    show various paths in one map

--- Folium/basic.py
+++ Folium/basic.py
@@ -95,3 +95,59 @@
 # 
 # m.save('basic.html')
 # =============================================================================
+
+
+#루트 합치기
+import osmnx as ox
+import networkx as nx
+import plotly.graph_objects as go
+import numpy as np
+import folium
+ox.config(use_cache=True, log_console=True)
+ox.__version__
+
+
+#원하는 지역 표시
+city = ox.graph_from_place('북구, 포항시, 경상북도, 대한민국')
+ox.plot_graph(ox.project_graph(city))
+
+#출발지, 도착지 노드 찾기
+orig_node = ox.get_nearest_node(city, (36.102960, 129.391312))
+dest_node = ox.get_nearest_node(city, (36.080058, 129.393611))
+
+dest_node2 = ox.get_nearest_node(city, (36.062193, 129.381536))
+
+#최단거리 분석
+route = nx.shortest_path(city, orig_node, dest_node, weight = 'length')
+route2 = nx.shortest_path(city, dest_node, dest_node2, weight = 'length')
+
+
+
+m = ox.plot_route_folium(city, route )
+ox.plot_route_folium(city, route2).add_to(m)
+#fig.add_to(m)
+#ax.add_to(m)
+#ox.plot_graph_route(city, route2).add_to(m)
+# Add the same marker to both maps:
+    
+# =============================================================================
+# fg = folium.FeatureGroup()
+# g1 = folium.FeatureGroupSubGroup(fg, 'g1')  # First subgroup of fg
+# g2 = folium.plugins.FeatureGroupSubGroup(fg, 'g2')  # Second subgroup of fg
+# m.add_child(fg)
+# m.add_child(g1)
+# m.add_child(g2)
+# g1.add_child(folium.Marker([0,0]))
+# g2.add_child(folium.Marker([0,1]))
+# folium.LayerControl().add_to(m) 
+# =============================================================================
+
+#fig, ax = ox.plot_graph_route(city, route2, node_size = 0)
+
+#fig, ax = ox.plot_graph_routes(city, routes, node_size=0)
+#m = ox.plot_route_folium(city, routes, node_size = 0)
+
+#m = ox.plot_route_folium(city, routes)
+m.save('basic.html')
+
+node, edge = city
